4352148739ae9d07b9b2c38e6fa44c363c5e6b73
Joseph Nguyen <joseph.nguyen@soaren-management.com>
2021-05-25 15:41:38 -0700

    initialize state

--- src/App.js
+++ src/App.js
@@ -1,68 +1,90 @@
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>RSVP</h1>
-        <p>A Treehouse App</p>
-        <form>
+class App extends Component {
+
+  state = {
+    guests: [
+      {
+        name: 'Treasure',
+        isConfirmed: false
+      },
+      {
+        name: 'Joseph',
+        isConfirmed: true
+      },
+    ]
+  }
+
+  getTotalInvited = () => this.state.guests.length;
+  // getAttendingGuests = () =>
+  // getUnconfirmedGuests = () =>
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>RSVP</h1>
+          <p>A Treehouse App</p>
+          <form>
             <input type="text" value="Safia" placeholder="Invite Someone" />
             <button type="submit" name="submit" value="submit">Submit</button>
-        </form>
-      </header>
-      <div className="main">
-        <div>
-          <h2>Invitees</h2>
-          <label>
-            <input type="checkbox" /> Hide those who haven't responded
-          </label>
+          </form>
+        </header>
+        <div className="main">
+          <div>
+            <h2>Invitees</h2>
+            <label>
+              <input type="checkbox" /> Hide those who haven't responded
+            </label>
+          </div>
+          <table className="counter">
+            <tbody>
+              <tr>
+                <td>Attending:</td>
+                <td>2</td>
+              </tr>
+              <tr>
+                <td>Unconfirmed:</td>
+                <td>1</td>
+              </tr>
+              <tr>
+                <td>Total:</td>
+                <td>3</td>
+              </tr>
+            </tbody>
+          </table>
+          <ul>
+            <li className="pending"><span>Safia</span></li>
+            <li className="responded"><span>Iver</span>
+              <label>
+                <input type="checkbox" checked /> Confirmed
+              </label>
+              <button>edit</button>
+              <button>remove</button>
+            </li>
+            <li className="responded">
+              <span>Corrina</span>
+              <label>
+                <input type="checkbox" checked /> Confirmed
+              </label>
+              <button>edit</button>
+              <button>remove</button>
+            </li>
+            <li>
+              <span>Joel</span>
+              <label>
+                <input type="checkbox" /> Confirmed
+              </label>
+              <button>edit</button>
+              <button>remove</button>
+            </li>
+          </ul>
         </div>
-        <table className="counter">
-          <tbody>
-            <tr>
-              <td>Attending:</td>
-              <td>2</td>
-            </tr>
-            <tr>
-              <td>Unconfirmed:</td>
-              <td>1</td>
-            </tr>
-            <tr>
-              <td>Total:</td>
-              <td>3</td>
-            </tr>
-          </tbody>
-        </table>
-        <ul>
-          <li className="pending"><span>Safia</span></li>
-          <li className="responded"><span>Iver</span>
-            <label>
-              <input type="checkbox" checked /> Confirmed
-            </label>
-            <button>edit</button>
-            <button>remove</button>
-          </li>
-          <li className="responded">
-            <span>Corrina</span>
-            <label>
-              <input type="checkbox" checked /> Confirmed
-            </label>
-            <button>edit</button>
-            <button>remove</button>
-          </li>
-          <li>
-            <span>Joel</span>
-            <label>
-              <input type="checkbox" /> Confirmed
-            </label>
-            <button>edit</button>
-            <button>remove</button>
-          </li>
-        </ul>
       </div>
-    </div>
-  );
+    );
+  }
+
 }
 
 export default App;
